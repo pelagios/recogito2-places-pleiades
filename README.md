c839ac0a9427bf65036336018744fca3985343ad
Rainer Simon <rainer.simon@ait.ac.at>
2017-04-03 11:17:10 +0200

    Various fixes to the script

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Recogito 2 Gazetteer Package: Pleiades
 
 Gazetteer data from the [Pleiades Gazetteer of the Ancient World](http://pleiades.stoa.org),
-packaged for use with [Recogito v.2](http://github.com/pelagios/recogito2). Includes [additional
-polygon shapes for regions](https://github.com/pelagios/gazetteer-data/tree/master/magis-pleiades-regions)
-digitized for the [MAGIS project](http://cgma.depauw.edu/MAGIS/), and made available by Pedar Foss
-for free redistribution (Copyright [AWMC](http://awmc.unc.edu), CC-BY).
+packaged for use with [Recogito v.2](http://github.com/pelagios/recogito2). The package is
+built from [Pleiades nightly JSON dumps](https://pleiades.stoa.org/downloads), but includes a few
+changes specifically for use with Recogito:
+
+- All place URLs are normalized to __http__, rather than __https__
+- Barrington Atlas grid rectangles are collapsed to a point for better visualization in Recogito
+- Regions are patched with geometries digitized by the [MAGIS project](http://cgma.depauw.edu/MAGIS/), and
+  made available by Pedar Foss for free redistribution (Copyright [AWMC](http://awmc.unc.edu), CC-BY).
+  ([Read more...](https://github.com/pelagios/gazetteer-data/tree/master/magis-pleiades-regions))
 
 ### Releases
 
--- buildJSONL.py
+++ buildJSONL.py
@@ -15,8 +15,34 @@ def loadRegions():
 
 def convertPlaces(regions):
 
-    ctrPlaces = 0
-    ctrMAGISUpdates = 0
+    def collapseRectangle(uri, features):
+        if len(features) > 0 and 'geometry' in features[0]:
+            geom = features[0]['geometry']
+            if geom and geom['type'] == 'Polygon':
+                coords = geom['coordinates'][0]
+                if len(coords) == 5:
+                    # A geometry with 5 coordinates? Might be a Barrington Atlas page
+                    if coords[0][0] == coords[1][0] and coords[0][1] == coords[3][1]:
+                        lon = (coords[0][0] + coords[2][0]) / 2
+                        lat = (coords[0][1] + coords[1][1]) / 2
+
+                        # Replace with point geometry
+                        features[0]['geometry'] = {
+                            'type': 'Point',
+                            'coordinates': [ lon, lat ]
+                        }
+
+                        return True
+        return False
+
+    # Counts all places
+    ctrPlaces    = 0
+
+    # Counts how many rectangles were collapsed
+    ctrCollapsed = 0
+
+    # Counts how many places were patched with MAGIS geometry
+    ctrMAGIS     = 0
 
     print('Building JSONL file')
     with gzip.open('data/pleiades-places-20170402.json.gz', 'rb') as f,  open('pleiades.jsonl', 'w') as out:
@@ -25,21 +51,31 @@ def convertPlaces(regions):
         for place in pleiades['@graph']:
             ctrPlaces += 1
 
+            # Normalize to http URIs
             uri = place['uri'].replace('https', 'http')
+            place['uri'] = uri
+
             if uri in regions:
-                ctrMAGISUpdates += 1
+                ctrMAGIS += 1
                 print('Updating geometry for ' + uri)
                 place['features'] = [{
                     'geometry': regions[uri],
                     'type': 'Features'
                 }]
+            else:
+                collapsed = collapseRectangle(uri, place['features'])
+                if collapsed:
+                    ctrCollapsed += 1
 
             out.write(json.dumps(place, ensure_ascii=False) + '\n')
 
         f.close()
         out.close()
 
-    print('Done. Converted ' + str(ctrPlaces) + ' places, patched ' + str(ctrMAGISUpdates) + ' regions.')
+    print('Done.')
+    print(str(ctrPlaces) + ' places converted')
+    print(str(ctrMAGIS) + ' regions patched')
+    print(str(ctrCollapsed) + ' rectangles collapsed')
 
 # Load MAGIS regions and build the JSONL file
 convertPlaces(loadRegions())
